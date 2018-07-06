5513d9f261ca6354be5baa47c23001f58f5c7175
Alex Saunders <alex.saunders@formidable.com>
2018-07-06 10:28:01 +0100

    Optimise isTouchable

--- src/util/isTouchable.js
+++ src/util/isTouchable.js
@@ -7,13 +7,14 @@
 import type { JSXOpeningElement } from 'ast-types-flow';
 import { elementType } from 'jsx-ast-utils';
 
-const touchables = [
-  'TouchableOpacity',
-  'TouchableHighlight',
-  'TouchableWithoutFeedback',
-  'TouchableNativeFeedback',
-];
+const touchables = {
+  TouchableOpacity: true,
+  TouchableHighlight: true,
+  TouchableWithoutFeedback: true,
+  TouchableNativeFeedback: true,
+  TouchableBounce: true,
+};
 
 export default function isTouchable(element: JSXOpeningElement) {
-  return new RegExp(`^(${touchables.join('|')})$`).test(elementType(element));
+  return touchables[elementType(element)] === true;
 }
