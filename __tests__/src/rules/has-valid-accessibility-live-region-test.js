cee5184b93cbda8e71ee4b1639880b9d6a6d1a87
Alex Saunders <alex.saunders@formidable.com>
2018-07-10 11:11:57 +0100

    Add tests

--- __tests__/src/rules/has-valid-accessibility-live-region-test.js
+++ __tests__/src/rules/has-valid-accessibility-live-region-test.js
@@ -20,13 +20,36 @@ import rule from '../../../src/rules/has-valid-accessibility-live-region';
 const ruleTester = new RuleTester();
 
 const expectedError = {
-  message: '',
-  type: 'JSXOpeningElement',
+  message: 'accessibilityLiveRegion must be one of defined values',
+  type: 'JSXAttribute',
 };
 
 ruleTester.run('has-valid-accessibility-live-region', rule, {
   valid: [
-    { code: '<div />;' },
+    { code: '<Text accessibilityLiveRegion="none">Click Me</Text>' },
+    { code: '<Text accessibilityLiveRegion="polite">Click Me</Text>' },
+    { code: '<Text accessibilityLiveRegion="assertive">Click Me</Text>' },
   ].map(parserOptionsMapper),
-  invalid: [].map(parserOptionsMapper),
+  invalid: [
+    {
+      code: '<Text accessibilityLiveRegion >Click Me</Text>',
+      errors: [expectedError],
+    },
+    {
+      code: '<Text accessibilityLiveRegion="">Click Me</Text>',
+      errors: [expectedError],
+    },
+    {
+      code: '<Text accessibilityLiveRegion="non">Click Me</Text>',
+      errors: [expectedError],
+    },
+    {
+      code: '<Text accessibilityLiveRegion="nones">Click Me</Text>',
+      errors: [expectedError],
+    },
+    {
+      code: '<Text accessibilityLiveRegion="button">Click Me</Text>',
+      errors: [expectedError],
+    },
+  ].map(parserOptionsMapper),
 });
