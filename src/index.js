fbe5162cf5cc5ad2f971c0b1d36c49e27c80567c
Alex Saunders <alex.saunders@formidable.com>
2018-07-10 12:15:54 +0100

    Add rule

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ module.exports = {
     'has-accessibility-props': require('./rules/has-accessibility-props'),
     'has-valid-accessibility-component-type': require('./rules/has-valid-accessibility-component-type'),
     'has-valid-accessibility-traits': require('./rules/has-valid-accessibility-traits'),
+    'has-valid-important-for-accessibility': require('./rules/has-valid-important-for-accessibility'),
     'no-nested-touchables': require('./rules/no-nested-touchables'),
   },
   configs: {
@@ -20,6 +21,7 @@ module.exports = {
         'react-native-a11y/has-accessibility-props': 'error',
         'react-native-a11y/has-valid-accessibility-component-type': 'error',
         'react-native-a11y/has-valid-accessibility-traits': 'error',
+        'react-native-a11y/has-valid-important-for-accessibility': 'error',
         'react-native-a11y/no-nested-touchables': 'error',
       },
     },
