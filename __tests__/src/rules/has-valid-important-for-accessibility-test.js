ade0ab7524ed80c3b18be928318480fbe3126898
Alex Saunders <alex.saunders@formidable.com>
2018-07-10 12:30:58 +0100

    Add tests

---
+++ __tests__/src/rules/has-valid-important-for-accessibility-test.js
@@ -0,0 +1,47 @@
+/* eslint-env jest */
+/**
+ * @fileoverview Enforce importantForAccessibility property value is valid
+ * @author Alex Saunders
+ */
+
+// -----------------------------------------------------------------------------
+// Requirements
+// -----------------------------------------------------------------------------
+
+import { RuleTester } from 'eslint';
+import parserOptionsMapper from '../../__util__/parserOptionsMapper';
+import rule from '../../../src/rules/has-valid-important-for-accessibility';
+
+// -----------------------------------------------------------------------------
+// Tests
+// -----------------------------------------------------------------------------
+
+const ruleTester = new RuleTester();
+
+const expectedError = {
+  message: 'importantForAccessibility must be one of defined values',
+  type: 'JSXAttribute',
+};
+
+ruleTester.run('has-valid-important-for-accessibility', rule, {
+  valid: [
+    { code: '<View importantForAccessibility="auto"/>' },
+    { code: '<View importantForAccessibility="yes"/>' },
+    { code: '<View importantForAccessibility="no"/>' },
+    { code: '<View importantForAccessibility="no-hide-descendants"/>' },
+  ].map(parserOptionsMapper),
+  invalid: [
+    {
+      code: '<View importantForAccessibility="" />',
+      errors: [expectedError],
+    },
+    {
+      code: '<View importantForAccessibility="aut" />',
+      errors: [expectedError],
+    },
+    {
+      code: '<View importantForAccessibility="autoyes" />',
+      errors: [expectedError],
+    },
+  ].map(parserOptionsMapper),
+});
