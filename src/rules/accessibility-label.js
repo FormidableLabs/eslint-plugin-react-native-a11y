b6180877d21e8e4443d192cbf49c8d16cc050fcf
Alex Saunders <alexjrsaunders@gmail.com>
2018-07-12 19:24:35 +0100

    Fix: Find accessibilityLabel in children (#26)
    Adds functionality to `accessibility-label` rule, which will now traverse children in order to find an `accessibilityLabel` value:
    
    <img width="768" alt="screen shot 2018-07-11 at 13 03 53" src="https://user-images.githubusercontent.com/22820481/42570166-ebb1ae6c-850a-11e8-9fa5-5bcc2a89106d.png">
    <img width="765" alt="screen shot 2018-07-11 at 13 04 04" src="https://user-images.githubusercontent.com/22820481/42570169-ec80ecae-850a-11e8-9ac9-7def9d333344.png">
    
    Fixes #17

--- src/rules/accessibility-label.js
+++ src/rules/accessibility-label.js
@@ -8,15 +8,24 @@
 // Rule Definition
 // ----------------------------------------------------------------------------
 
-import type { JSXOpeningElement } from 'ast-types-flow';
 import { getProp } from 'jsx-ast-utils';
 import getLiteralPropValue from 'jsx-ast-utils/lib/getPropValue';
+import type { JSXOpeningElement } from 'ast-types-flow';
 import type { ESLintContext } from '../../flow/eslint';
 import { generateObjSchema } from '../util/schemas';
+import findChild from '../util/findChild';
 
-const errorMessage = 'If an element has accessible={true}, it must also have a (non-empty) accessibilityLabel prop';
+const errorMessage = 'If an element adopts the accessible={true} prop, it (or at least one of its children) must also set the accessibilityLabel prop';
 
 const schema = generateObjSchema();
+
+function getAccessibilityLabel(node) {
+  const labelProp = getProp(node.attributes, 'accessibilityLabel');
+  if (labelProp && labelProp.value) {
+    return getLiteralPropValue(labelProp);
+  }
+  return null;
+}
 
 module.exports = {
   meta: {
@@ -28,20 +37,25 @@ module.exports = {
     JSXOpeningElement: (node: JSXOpeningElement) => {
       const accessible = getProp(node.attributes, 'accessible');
       if (accessible) {
-        const labelProp = getProp(node.attributes, 'accessibilityLabel');
-        if (labelProp && labelProp.value) {
-          const labelPropVal = getLiteralPropValue(labelProp);
-          if (!labelPropVal) {
+        const labelPropVal = getAccessibilityLabel(node);
+        if (!labelPropVal) {
+          let childWithLabel;
+          if (node.parent) {
+            // $FlowFixMe
+            childWithLabel = findChild(node.parent, (child) => {
+              if (child.attributes) {
+                const childLabelValue = getAccessibilityLabel(child);
+                return !!childLabelValue;
+              }
+              return false;
+            });
+          }
+          if (!childWithLabel) {
             context.report({
               node,
               message: errorMessage,
             });
           }
-        } else {
-          context.report({
-            node,
-            message: errorMessage,
-          });
         }
       }
     },
