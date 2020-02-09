0c7823816811d456846baac1139a0a17ed08aa9b
JP <jpdriver@users.noreply.github.com>
2020-02-09 11:29:45 +0000

    fixup Flow errors

--- src/util/isNodePropValueBoolean.js
+++ src/util/isNodePropValueBoolean.js
@@ -1,24 +1,27 @@
 // @flow
+import type { JSXAttribute } from 'ast-types-flow';
 
-export default function isNodePropValueBoolean(node): boolean {
+export default function isattrPropValueBoolean(attr: JSXAttribute): boolean {
   /**
-   * Using `typeof getLiteralPropValue(node) === 'boolean'` with getLiteralPropValue/getPropValue
+   * Using `typeof getLiteralPropValue(attr) === 'boolean'` with getLiteralPropValue/getPropValue
    * from `jsx-ast-utils` doesn't work as expected, because it "converts" strings `"true"` and `"false"`
    * to booleans `true` and `false`. This function aims to correctly identify uses of the string instead
    * of the boolean, so that we can correctly identify this error.
    */
 
-  if (typeof node !== 'object' || !node.hasOwnProperty('value')) {
+  if (typeof attr !== 'object' || !attr.hasOwnProperty('value')) {
     // Loose check for correct data being passed in to this function
-    throw new Error('isNodePropValueBoolean expects a node object as argument');
+    throw new Error('isattrPropValueBoolean expects a attr object as argument');
   }
-  if (node.value === null) {
-    // node.value is null when it is declared as a prop but not equal to anything. This defaults to `true` in JSX
+  if (attr.value === null) {
+    // attr.value is null when it is declared as a prop but not equal to anything. This defaults to `true` in JSX
     return true;
   }
-  if (node.value.expression.type !== 'Literal') {
+  // $FlowFixMe
+  if (attr.value.expression.type !== 'Literal') {
     // If not a literal, it cannot be a boolean
     return false;
   }
-  return typeof node.value.expression.value === 'boolean';
+  // $FlowFixMe
+  return typeof attr.value.expression.value === 'boolean';
 }
